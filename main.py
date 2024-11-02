4715fc53402d5be99d6bb138ba65d058f07711c9
HeroinPic <piccinimarco96@gmail.com>
2024-11-02 19:24:10 +0100

    added char counting def

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     print(text)
     count = words_count(text)
     print(f"The Frankenstein book counts '{count}' words")
+    print(character_count(text))
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -15,6 +17,17 @@ def words_count(book):
     for word in book.split():
         count += 1
     return count
+
+
+def character_count(book):
+    character_dict = {}
+    for char in book:
+        lowered = char.lower()
+        if lowered in character_dict:
+            character_dict[lowered] += 1
+        else:
+            character_dict[lowered] = 1
+    return character_dict
 
 
 main()
